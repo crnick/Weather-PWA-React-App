e9eced9b35d715f468ce6140c9f1921bedc941c3
nikhil joglekar <nick.nj94@gmail.com>
2023-12-18 22:35:10 +0530

    added offline.html file and updated serviceworker file for fetching and activate Service worke

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -4,13 +4,37 @@ const urlsToCache = ["index.html", "offline.html"];
 //listen to installation event of service worker
 this.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => { 
-      cache.addAll(urlsToCache); 
+    caches.open(CACHE_NAME).then((cache) => {
+      cache.addAll(urlsToCache);
     })
   );
 });
 //listen to requests
-this.addEventListener("fetch", () => {});
+this.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches
+      .match(event.request) // request for api call, or for an image
+      .then(() => {
+        return fetch(event.request);
+      })
+      .catch(() => caches.match("offline.html"))
+  ); // if we don't get the resource that we want, returns an error
+});
 
 //active service worker
-this.addEventListener("activate", () => {});
+this.addEventListener("activate", (event) => {
+  //we are going to remove caches
+  const cacheWhitelist = [];
+  cacheWhitelist.push(CACHE_NAME); // what we want to keep
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName); // will delete any new versions and always keep version-1
+          }
+        })
+      )
+    )
+  );
+});
